97dbceb26f88d06c25187243dc19ca7701cee074
javiosyc <javiosyc@gmail.com>
2017-10-31 03:49:03 +0800

    get outdir from system property
    Change-Id: I8b6c56a776e499039b9b8b5c97f34a50de2daa00

--- generator/src/test/java/generator/utils/AppiumTestGeneratorTest.java
+++ generator/src/test/java/generator/utils/AppiumTestGeneratorTest.java
@@ -8,10 +8,14 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import javax.lang.model.element.Modifier;
+import javax.management.RuntimeErrorException;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -35,13 +39,19 @@ public class AppiumTestGeneratorTest {
 	@Test
 	public void test() throws FileNotFoundException, IOException {
 
+		String outDir = System.getProperty("outdir");
+
+		if (StringUtils.isBlank(outDir)) {
+			throw new RuntimeException("need to set outdir");
+		}
+
 		ExcelReader reader = new ExcelReader(path);
 
 		reader.test();
 
 		AppiumTestGenerator generator = new AppiumTestGenerator(reader);
 
-		generator.setOutputDir("/Users/esunbank/test-script/appium-script/test/src/test/java");
+		generator.setOutputDir(outDir);
 
 		generator.generate();
 
